32a7afc1c63419bbb7dfb84ae962763d5929040f
Anshuman <anshuman8800@gmail.com>
2022-02-28 18:14:34 +0530

    Initialized React and MUI components

---
+++ web_frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {Button} from '@material-ui/core'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>headind</h1>
+      <Button>click me</Button>
+    </div>
+  );
+}
+
+export default App;
